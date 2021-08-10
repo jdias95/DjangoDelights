ebb3acf4e59968fd890e077983e56c92cf090d3a
AkomaMathaino <dias.joshua7@yahoo.com>
2021-08-10 13:23:55 -0700

    Edit Models

--- inventory/models.py
+++ inventory/models.py
@@ -11,35 +11,38 @@ class Ingredient(models.Model):
     MEASUREMENTS = [
         ('tsp', 'teaspoon'),
         ('tbsp', 'tablespoon'),
+        ('g', 'grams'),
+        ('oz', 'ounce'),
         ('c', 'cup'),
         ('pt', 'pint'),
         ('qt', 'quart'),
         ('gal', 'gallon'),
-        ('lb', 'pound')
+        ('lb', 'pound'),
+        ('None', None)
     ]
     unit = models.CharField(max_length=10, choices=MEASUREMENTS)
     unit_price = models.FloatField()
 
     def __str__(self):
-        return self.name + " quantity: " + str(self.quantity) + "\n" + "price per " + self.unit + ": $" + str(self.unit_price)
+        return self.name + " quantity: " + str(self.quantity) + ",\n" + "price per " + self.unit + ": $" + str(format(self.unit_price, '.2f')) if (self.unit != 'None') else self.name + " quantity: " + str(self.quantity) + ",\n" + "price per: $" + str(format(self.unit_price, '.2f'))
 
-class MenuItems(models.Model):
+class MenuItem(models.Model):
     title = models.CharField(max_length=30)
     price = models.FloatField()
 
     def __str__(self):
-        return self.title + " costs $" + self.price
+        return self.title + " costs $" + str(format(self.price, '.2f')) 
 
 class RecipeRequirement(models.Model):
-    menu_item = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
     def __str__(self):
-        return "Menu Item with ID of " + str(self.menu_item) + " requires " + str(self.quantity) + " of its unit of the ingredient with the ID " + str(self.ingredient)
+        return str(self.menu_item) + " and requires " + str(self.quantity) + " of its unit of " + str(self.ingredient)
 
 class Purchase(models.Model):
-    menu_item = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
 
     def __str__(self):

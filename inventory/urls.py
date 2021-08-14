b9eb10d244581e68bbfe063ee5419c8efe2dba41
AkomaMathaino <dias.joshua7@yahoo.com>
2021-08-14 14:56:48 -0700

    Create templates for listviews and ingredient deleteview as well as styling

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('ingredient/list', views.IngredientList.as_view(), name='ingredientlist'),
     path('ingredient/delete/<pk>', views.IngredientDelete.as_view(), name='ingredientdelete'),
     path('menuitem/list', views.MenuItemList.as_view(), name='menuitemlist'),
-    path('purchase/list', views.PurchaseList.as_view(), name='purchaselist')
+    path('purchase/list', views.PurchaseList.as_view(), name='purchaselist'),
+    path('report', views.profit_report, name='report'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -9,17 +9,20 @@ def home(request):
 
 class IngredientList(ListView):
     model = Ingredient
-
+    template_name = "inventory/inventory.html"
 
 class IngredientDelete(DeleteView):
     model = Ingredient
-
+    template_name = "inventory/ingredient_delete_form.html"
 
 class MenuItemList(ListView):
     model = MenuItem
-
+    template_name = "inventory/menu.html"
 
 class PurchaseList(ListView):
     model = Purchase
+    template_name = "inventory/purchases.html"
 
+def profit_report(request):
+    return render(request, "inventory/report.html")
 
